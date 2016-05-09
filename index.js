cb33d55504b73c88b85fdd4f2a79230a263b7bd3
BeroMaze <Berning.corey@gmail.com>
2016-05-09 13:48:40 -0700

    push info to server

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 // "use strict";
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var PORT = 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('./'));
+app.use(bodyParser());
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.post('/userLocation', function(req, res) {
+  var userInfo = req.body.userper;
+  console.log(userInfo);
+  res.send('send');
+});
+
 
 app.listen(PORT, function(){
   console.log('listen on port:'+ PORT);
