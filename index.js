4995d9f41392a0409685cec7e3450ee483a935ce
BeroMaze <Berning.corey@gmail.com>
2016-05-09 12:55:50 -0700

    server running

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-"use strict";
+// "use strict";
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var PORT = 3000;
-
-
-
 
 app.listen(PORT, function(){
   console.log('listen on port:'+ PORT);
   console.log('server running');
-})
+});
